c0fce2372338760743606423ac2ab4da35234d5f
Marcos Paliar <marcos@paliari.com.br>
2014-03-15 11:11:43 -0300

    @dev adiciona tests dos filters

---
+++ tests/Paliari/TextObject/Filters/FDateTest.php
@@ -0,0 +1,89 @@
+<?php
+use Paliari\TextObject\Filters\FDate;
+
+/**
+ * Class FDateTest
+ */
+class FDateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequired()
+    {
+        $d = new FDate(true);
+        $this->assertEquals('', $d(''));
+    }
+
+    /**
+     * Se a data nao e obrigatorio.
+     */
+    public function testNoRequired()
+    {
+        $d = new FDate();
+        $this->assertEquals('', $d(''));
+        $this->assertEquals(new DateTime('2014-01-01'), $d('2014-01-01'));
+    }
+
+    /**
+     * Testa datas num periodo de 10 anos.
+     * 
+     * @group long
+     */
+    public function testPeriodo()
+    {
+        $stop = date('Y', time()) + 5;
+        $ano  = date('Y', time()) - 5;
+        $mes  = 1;
+        $dia  = 0;
+        $f    = new FDate();
+        while ($ano <= $stop) {
+            $timestamp = mktime(0, 0, 0, $mes, $dia + 1, $ano);
+            $dia       = date('d', $timestamp);
+            $mes       = date('m', $timestamp);
+            $ano       = date('Y', $timestamp);
+            $f->setFormat('YYYY-MM-DD');
+            $this->assertEquals(new DateTime("$ano-$mes-$dia"), $f("$ano-$mes-$dia"));
+            $f->setFormat('DD/MM/YYYY');
+            $this->assertEquals(new DateTime("$ano-$mes-$dia"), $f("$dia-$mes-$ano"));
+            $f->setFormat('DDMMYYYY');
+            $this->assertEquals(new DateTime("$ano-$mes-$dia"), $f("$dia$mes$ano"));
+            $f->setFormat('YYYYMMDD');
+            $this->assertEquals(new DateTime("$ano-$mes-$dia"), $f("$ano$mes$dia"));
+        }
+
+    }
+
+    /**
+     * testa com data time de um dia inteiro a cada segundo.
+     *
+     * @group long
+     */
+    public function testPeriodoTime()
+    {
+        $y = date('Y', time());
+        $m = date('m', time());
+        $d = date('m', time());
+        $h = 0;
+        $i = 0;
+        $s = 0;
+
+        $stp = $d + 1;
+        $f   = new FDate();
+
+        while ($d < $stp) {
+            $tmstp = mktime($h, $i, $s + 1, $m, $d, $y);
+            $d     = str_pad(date('d', $tmstp), 2, '0', STR_PAD_LEFT);
+            $m     = str_pad(date('m', $tmstp), 2, '0', STR_PAD_LEFT);
+            $y     = str_pad(date('Y', $tmstp), 2, '0', STR_PAD_LEFT);
+            $h     = str_pad(date('H', $tmstp), 2, '0', STR_PAD_LEFT);
+            $i     = str_pad(date('i', $tmstp), 2, '0', STR_PAD_LEFT);
+            $s     = str_pad(date('s', $tmstp), 2, '0', STR_PAD_LEFT);
+
+            $f->setFormat('YYYYMMDDHHIISS');
+            $this->assertEquals(new DateTime("$y-$m-$d $h:$i:$s"), $f("$y$m$d$h$i$s"));
+            $f->setFormat('DDMMYYYYHHIISS');
+            $this->assertEquals(new DateTime("$y-$m-$d $h:$i:$s"), $f("$d$m$y$h$i$s"));
+        }
+    }
+}
---
+++ tests/Paliari/TextObject/Filters/FiltersTest.php
@@ -0,0 +1,106 @@
+<?php
+use Paliari\TextObject\Filters\FDate,
+    Paliari\TextObject\Filters\FEmail,
+    Paliari\TextObject\Filters\FString,
+    Paliari\TextObject\Filters\FDouble,
+    Paliari\TextObject\Filters\FInt,
+    Paliari\TextObject\Filters\FNumberString;
+
+/**
+ * Class FDate
+ */
+class FiltersTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Testa se todos os models estão sendo instanciados.
+     */
+    public function testInstance()
+    {
+        $diretorio = ROOT . '/src/Paliari/TextObject/Filters';
+        $ponteiro  = opendir($diretorio);
+
+        while ($nome_itens = readdir($ponteiro)) {
+            $className = explode('.', $nome_itens)[0];
+            if ('' !== $className && 'AbstractFilter' !== $className) {
+                $className = "Paliari\\TextObject\\Filters\\$className";
+                $classe = new $className();
+                $this->assertTrue($classe instanceof $className);
+                $this->assertFalse((bool)$classe(''));
+            }
+        }
+    }
+
+    /**
+     * Testa para campos nao obrigatorios
+     */
+    public function testNoRequired()
+    {
+        $f = new FDate();
+        $this->assertEquals('', $f(''));
+        $f = new FEmail();
+        $this->assertEquals('', $f(''));
+        $f = new FString();
+        $this->assertEquals('', $f(''));
+        $f = new FNumberString();
+        $this->assertEquals('', $f(''));
+        $f = new FInt();
+        $this->assertEquals(0, $f(''));
+        $f = new FDouble();
+        $this->assertEquals(0, $f(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredDate()
+    {
+        $d = new FDate(true);
+        $this->assertEquals('', $d(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredDouble()
+    {
+        $d = new FDouble(true);
+        $this->assertEquals('', $d(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredEmail()
+    {
+        $d = new FEmail(true);
+        $this->assertEquals('', $d(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredInt()
+    {
+        $d = new FInt(true);
+        $this->assertEquals(0, $d(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredNumericString()
+    {
+        $d = new FNumberString(true);
+        $this->assertEquals('', $d(''));
+    }
+
+    /**
+     * @expectedException DomainException
+     */
+    public function testRequiredString()
+    {
+        $d = new FString(true);
+        $this->assertEquals('', $d(''));
+    }
+}
